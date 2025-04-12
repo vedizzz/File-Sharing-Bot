fc66d9c4323e0c4c78dfc00c8f2b5ce3a7a890b6
vedizzz <nithishtk6@gmail.com>
2025-04-12 08:21:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,7 +63,7 @@ AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has bee
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❌Don't send me messages Join @Warmovietamilss or @Tamil_LinksZzz"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(5906206984)
